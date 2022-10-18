60d10a4eaa48e87940ade0ed2365f7f4478cfa1b
richard <theirsaviorhd@gmail.com>
2022-10-18 14:02:30 -0400

    Grab User Input and Covert To Something Readable
    grab user inupt from prompt
    convert user input to lowercase
    convert lowercase result to a number

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,13 +1,46 @@
+//computer chooses between rock paper or scissors
 function getComputerChoice() {
 let computerMove = Math.floor(Math.random() * 3);
 if (computerMove === 0) {
-return "Computer used rock."
+return 0;
 }
 else if (computerMove === 1) {
-return "Computer used paper."
+return 1;
 }
 else 
-return "Computer used scissors."
+return 2;
 }
+//console.log(getComputerChoice())
 
-console.log(getComputerChoice())
+//converts user's input to lowercase
+function playerSelection(){
+let userInput = prompt("rock paper or scissors?");
+let revisedInput = userInput.toLowerCase();
+return revisedInput;
+}
+//console.log(playerSelection())
+
+//converted input to number
+function userNumber() {
+    let userActualNumber = playerSelection();
+    if (userActualNumber === "rock") {
+        return 0;
+    } else if (userActualNumber === "paper") {
+        return 1;
+    } else if (userActualNumber === "scissors") {
+        return 2;
+    } else {
+        return "use a valid input"
+    }
+}
+//console.log(userNumber())
+
+//compare both inputs
+function compareInputs() {
+    let computerInput = getComputerChoice();
+    let playerInput = userNumber();
+
+    if (computerInput === playerInput) {
+        return "Tie"
+    }
+}
